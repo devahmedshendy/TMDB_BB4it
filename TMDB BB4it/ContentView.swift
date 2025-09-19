f5b44325a8cc9b4f85fee4c960d5ef092698ea70
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2025-09-19 07:33:37 +0300

    Initial Commit

---
+++ TMDB BB4it/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TMDB BB4it
+//
+//  Created by Ahmed Shendy on 19/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TMDB BB4it/TMDB_BB4itApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_BB4itApp.swift
+//  TMDB BB4it
+//
+//  Created by Ahmed Shendy on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_BB4itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
